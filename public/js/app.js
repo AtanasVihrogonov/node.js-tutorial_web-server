f2b4bd806463a61ed8b18b963daf55cd2330c654
Atanas Vihrogonov <vihrogonov@protonmail.com>
2020-07-09 10:24:30 +0100

    UI

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,29 @@
 console.log('Client side javascript file is loaded!');
 
-fetch('http://localhost:3000/weather?address=!').then((response) => {
+const weaderForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+// messageOne.textContent = 'From JavaScript';
+// messageTwo.textContent = 'From Node.js';
+
+weaderForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const location = search.value;
+
+  messageOne.textContent = 'Loading...';
+  messageTwo.textContent = '';
+
+  fetch('http://localhost:3000/weather?address=' + location).then((response) => {
   response.json().then((data) => {
     if (data.error) {
-      console.log(data.error);
+      messageOne.textContent = data.error;
     } else {
-      console.log(data.location);
-      console.log(data.forecast);
+      messageOne.textContent = data.location;
+      messageTwo.textContent = data.forecast;
     }
   });
+});
 });
